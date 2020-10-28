a68500a03839e6ab99f3f9bfa5df49787663e99f
byron7cueva <byron7cueva@gmail.com>
2020-10-28 16:49:34 -0500

    Add docs

--- basics/hello-world.ts
+++ basics/hello-world.ts
@@ -1,9 +1,14 @@
 // deno run --allow-net hello-world.ts 
+// deno run --allow-net=0.0.0.0:8000 hello-world.ts 
 import { serve } from 'https://deno.land/std@0.75.0/http/server.ts';
 
 const s = serve({ port: 8000 });
 
 console.log("http://localhost:8000");
+
+// Async iterators
+// Cada vez que se resiva un request se ejcuta lo
+// siguiente
 for await (const req of s) {
   req.respond({ body: "Hello world"});
 }
